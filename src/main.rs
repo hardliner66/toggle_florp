fdc8f28ce36c383dcf02e2e63f445f41bf9fd224
hardliner66 <hardliner66@gmail.com>
2020-11-16 11:44:49 +0100

    made program a single expression

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-use rand::prelude::*;
-
 fn main() {
-    let mut rng = rand::thread_rng();
-    let r: u8 = rng.gen();
-    let g: u8 = rng.gen();
-    let b: u8 = rng.gen();
-    println!("The color of the florp shall be:\n{} {} {}", r, g, b);
+    println!(
+        "The color of the florp shall be:\n{} {} {}",
+        rand::random::<u8>(), // r
+        rand::random::<u8>(), // g
+        rand::random::<u8>()  // b
+    );
 }
